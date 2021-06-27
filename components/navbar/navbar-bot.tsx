146301fe3ab3143c52c207d9a4ca962921399570
alex <george.marcu20@gmail.com>
2021-06-27 10:55:25 +0300

    centered navbar and sized logo

--- components/navbar/navbar-bot.tsx
+++ components/navbar/navbar-bot.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { NavbarBrandProps } from "react-bootstrap";
 import { Button, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
-
+import Image from "next/image";
 
 const NavBot = () => {
     return (
@@ -13,17 +13,17 @@ const NavBot = () => {
     <Navbar.Toggle aria-controls="basic-navbar-nav"  />
     <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
         
-      <Nav className="ml-auto justify-content-between">
+      <Nav className="ml-auto  justify-content-between">
          <>
-         <Navbar.Brand className="  d-lg-none d-md-none d-sm-block"  href="#home">
-             <img src="/assets/images/Phone-layout.webp"  alt="logo+phone" />
+         <Navbar.Brand className="  my-2 d-lg-none d-md-none d-sm-block"  href="#home">
+             <Image src="/assets/images/Phone-layout.webp"  alt="logo+phone" width={300} height={50} layout='responsive' />
          </Navbar.Brand>
          </>
-        <Nav.Link href="/">Acasa</Nav.Link>
-        <Nav.Link href="#servicii">Servicii</Nav.Link>
-        <Nav.Link href="/consultanta-online">Consultanta Online</Nav.Link>
-        <Nav.Link href="/blog">Blog</Nav.Link>
-        <Nav.Link href="/contact">Contact</Nav.Link>
+        <Nav.Link className='align-self-center' href="/">Acasa</Nav.Link>
+        <Nav.Link className='align-self-center' href="#servicii">Servicii</Nav.Link>
+        <Nav.Link className='align-self-center' href="/consultanta-online">Consultanta Online</Nav.Link>
+        <Nav.Link className='align-self-center' href="/blog">Blog</Nav.Link>
+        <Nav.Link className='align-self-center' href="/contact">Contact</Nav.Link>
       </Nav>
  
     </Navbar.Collapse>
---
+++ pages/drept-penal.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const penal=()=>{
+    return(
+        <h1>drept-penal</h1>
+    )
+}
+
+export default penal
