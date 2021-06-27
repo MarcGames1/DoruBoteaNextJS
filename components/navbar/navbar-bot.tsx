4b0701c945a1de5e803d155b0b82873493741285
alex <george.marcu20@gmail.com>
2021-06-27 19:22:06 +0300

    moved navbar-togle to right

--- components/navbar/navbar-bot.tsx
+++ components/navbar/navbar-bot.tsx
@@ -1,7 +1,6 @@
 // afisare conditionata cu varianta navbar phone
 import React from "react";
-import { NavbarBrandProps } from "react-bootstrap";
-import { Button, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import {  Container, Nav, Navbar } from "react-bootstrap";
 import Image from "next/image";
 
 const NavBot = () => {
@@ -9,14 +8,16 @@ const NavBot = () => {
         <>
     <Navbar  bg="secondary-color" variant="dark"  expand="md">
   <Container  >
-   
-    <Navbar.Toggle aria-controls="basic-navbar-nav"  />
-    <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
+   <Container fluid>
+    <Navbar.Toggle className="mr-auto  align-self-end "  aria-controls="basic-navbar-nav"  />
+    </Container>
+    <Navbar.Collapse id="basic-navbar-nav">
         
-      <Nav className="ml-auto  justify-content-between">
+      <Nav className="justify-content-between">
          <>
          <Navbar.Brand className="  my-2 d-lg-none d-md-none d-sm-block"  href="#home">
              <Image src="/assets/images/Phone-layout.webp"  alt="logo+phone" width={300} height={50} layout='responsive' />
+            
          </Navbar.Brand>
          </>
         <Nav.Link className='align-self-center' href="/">Acasa</Nav.Link>
