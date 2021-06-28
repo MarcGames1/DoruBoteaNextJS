9b3f5d41be63532c40bd1f51091b5afe02d4fe70
alex <george.marcu20@gmail.com>
2021-06-28 13:59:40 +0300

    dreptul-familiei

--- pages/dreptul-familiei.tsx
+++ pages/dreptul-familiei.tsx
@@ -4,17 +4,74 @@ import SecondaryPage from "../components/SecondaryPage";
 const page=()=>{
     return(<>
         <SecondaryPage 
-        metaTitle={'DREPT PENAL'}
+        metaTitle={'Dreptul Familiei'}
         metaDescription={'Descriere'}
-        h1={'DREPT PENAL'}
+        h1={'Dreptul familiei'}
         >
+                        <div className="text">
+                
+                <p>Procesul de divort implica de cele mai multe ori atat decizii greu de luat, precum si stari
+                    emotionale
+                    dificil de surmontat pentru ambele parti implicate.
+                    Divortul reprezinta, in primul rand, o oficializare a unei rupturi definitive, acumulata in timp,
+                    intre
+                    cei
+                    doi soti. Din punct de vedere legal, insa, divortul poate implica o serie de alte aspecte deosebit
+                    de
+                    importante si de sensibile, care nu se pot solutiona decat prin derularea unei consultante juridice
+                    acordata
+                    de avocat:</p>
+                <ul>
+                    <li>incredintarea minorilor din casatorie</li>
+                    <li>stabilirea pensiei de intretinere pentru minorii din casatorie</li>
+                    <li>partajarea bunurilor comune din casatorie – partaj</li>
+                    <li>modalitatea de desfacere a casatoriei</li>
+                    <li>exercitarea autoritatii parintesti</li>
+                    <li>stabilirea domiciliului minorilor</li>
+                    <li>ce nume se va purta dupa desfacerea casatoriei</li>
+                    <li>stabilirea pensiei de intretinere in favoarea minorului/minorilor</li>
+                </ul>
+                <h2 className='h3 mt-3'>Cu ce te ajuta un avocat in procesul de divort</h2>
+                <p>Asistenta unui avocat in procesul de divort este imperios necesara, chiar daca legea nu o impune.
 
+
+                    Cunoasterea in detaliu a elementelor ce definesc un astfel de proces, a modului in care fiecare
+                    parte
+                    isi
+                    poate sustine si apara punctual de vedere, efectuarea in cunostinta de cauza si in mod corect a
+                    etapelor
+                    si
+                    procedurilor ce decurg firesc dintr-un astfel de proces, reprezinta impreuna elementele absolut
+                    necesare
+                    si
+                    legale pentru obtinerea unei decizii de divort echitabila si in conformitate cu vointa ambelor parti
+                    si
+                    cu
+                    situatia reala existenta.</p>
+                <h2 className='h3 mt-3'>Avand o bogata experienta in Dreptul Familiei si in Dreptul Civil, Cabinetul de Avocatura Botea Doru
+                    desfasoara in mod uzual activitati de:</h2>
+                <ul>
+                    <li>consultanta juridica pentru procese de divort</li>
+                    <li>asistenta juridica a partilor pe durata proceselor de divort</li>
+                    <li>reprezentare a cetatenilor romani din strainatate in procese de divort desfasurate in Romania.
+                        Trebuie mentionat ca in acest caz nu se poate realiza divortul la notar</li>
+                    <li>redactare documente juridice necesare in procesele de divort</li>
+                    <li>realizarea partajului o data cu procesul de divort</li>
+                </ul>
+                <p>Cabinetul de Avocat Botea Doru se straduieste cu consecventa sa ia cele mai bune decizii in
+                    realizarea
+                    unui proces de divort cu implicatii cat mai reduse emotional si financiar pe toata durata
+                    procesului,
+                    cat si ulterior, tinand cont de consecintele firesti ale acestui process legal.
+                </p>
+                <strong> Profesionalism si Preturi Decente.</strong>
+            </div>
 </SecondaryPage> 
 
 
 <style jsx global>{`
 .parallax{
-background-image: url('/penal.png');
+background-image: url('/familie.jpeg');
 }
     
 `}</style>
