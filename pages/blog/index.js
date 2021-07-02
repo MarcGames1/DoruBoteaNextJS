9b3109977fa33ccdabaaaa78cc614cbd1af80ac1
alex <george.marcu20@gmail.com>
2021-07-02 12:05:23 +0300

    blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,72 +1,73 @@
 import React from "react";
 import Link from "next/link";
-import Image from 'next/image'  ;
-import { Container,CardGroup, Card, Button } from "react-bootstrap";
+import Image from 'next/image';
+import { Container, CardGroup, Card, Button } from "react-bootstrap";
 
 
 function IndexPage(props) {
   return (
-<>
-<h1>Blog</h1>
-<Container fluid>
-  <CardGroup>
-{props.blogs.map((blog,idx )=>{
-  
-  
-  return(
-    <Card key={idx}>
-    <div className='card-img-top'>
-   <Image  layout='fill' objectFit='scale-down' src={blog.img} />
-   </div>
-  <Card.Body>
-    <Card.Title>{blog.title}</Card.Title>
-    <Card.Text>
-      {blog.description}
-    </Card.Text>
-    <Link href={`/blog/${blog.slug}`}>
-    <Button variant="primary">Citeste Articolul</Button>
-    </Link>
-  </Card.Body>
-</Card>
-  )
-})}
-</CardGroup>
-</Container>
-</>
-  //   <a className = "my-5">
-  //       <a className="container bg-primary-color" >
-  //       <section className="text-center">
-  //       <h4 className="mb-5"><strong>Blog</strong></h4>
-  //       <div className="row">
-  //     <a className="row">
-  //       {props.blogs.map((blog, idx) => {
-  //           return (
-  //             <a className="col-lg-4 col-md-12 mb-4">
-  //               <a key={blog.id}>
-  //           <a className="card transparent-card">
-  //             <a className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-  //               <Image src={blog.img} className="img-fluid" layout="responsive" height={300} width= {150} object-fit= "contain" />
-               
-  //                 <a className="mask" style={{backgroundColor:"rgba(251, 251, 251, 0.15)"}}></a>
+    <>
+      <h1>Blog</h1>
+      <Container fluid>
+        <CardGroup>
+          {props.blogs.map((blog, idx) => {
+
+
+            return (
+              <Card  key={idx}>
+                  <Card.Title>{blog.title}</Card.Title>
                 
-  //             </a>
-  //             <a className="card-body">
-  //               <h5 className="text-secondary card-title">{blog.title}</h5>
-  //               <p className="card-text">{blog.description} </p>
-  //               <Link href={`/blog/${blog.slug}`}>
-  //               <a href="#!" className="text-secondary btn from-left">Cititi</a>
-  //               </Link>
-  //             </a>
-  //           </a>
-  //           </a>
-  //         </a>
-  //         );
-  //       })}
-  //     </a>
-  //       </div>
-  //       </section>
-  //     </a>
-  //   </a>
+               
+                <img className='card-img-top' height={300} src={blog.img}  />
+                <Card.Body>
+                  <Card.Text>
+                    {blog.description}
+                  </Card.Text>
+                  <Link href={`/blog/${blog.slug}`}>
+                    <Button variant="primary">Citeste Articolul</Button>
+                  </Link>
+                </Card.Body>
+                <Card.Footer className="text-muted">data postarii: {blog.date}</Card.Footer>
+              </Card>
+            )
+          })}
+        </CardGroup>
+      </Container>
+    </>
+    //   <a className = "my-5">
+    //       <a className="container bg-primary-color" >
+    //       <section className="text-center">
+    //       <h4 className="mb-5"><strong>Blog</strong></h4>
+    //       <div className="row">
+    //     <a className="row">
+    //       {props.blogs.map((blog, idx) => {
+    //           return (
+    //             <a className="col-lg-4 col-md-12 mb-4">
+    //               <a key={blog.id}>
+    //           <a className="card transparent-card">
+    //             <a className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+    //               <Image src={blog.img} className="img-fluid" layout="responsive" height={300} width= {150} object-fit= "contain" />
+
+    //                 <a className="mask" style={{backgroundColor:"rgba(251, 251, 251, 0.15)"}}></a>
+
+    //             </a>
+    //             <a className="card-body">
+    //               <h5 className="text-secondary card-title">{blog.title}</h5>
+    //               <p className="card-text">{blog.description} </p>
+    //               <Link href={`/blog/${blog.slug}`}>
+    //               <a href="#!" className="text-secondary btn from-left">Cititi</a>
+    //               </Link>
+    //             </a>
+    //           </a>
+    //           </a>
+    //         </a>
+    //         );
+    //       })}
+    //     </a>
+    //       </div>
+    //       </section>
+    //     </a>
+    //   </a>
   );
 }
 
@@ -90,11 +91,11 @@ export async function getStaticProps() {
       return { ...data, id: uuid() };
     });
 
-    // By returning { props: blogs }, the IndexPage component
+  // By returning { props: blogs }, the IndexPage component
   // will receive `blogs` as a prop at build time
   return {
     props: { blogs },
   };
 }
-<img src= "/assets"/>
+<img src="/assets" />
 export default IndexPage;
