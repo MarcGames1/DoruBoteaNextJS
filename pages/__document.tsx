900a1bf9885fd27c7adea46e51d8517040face5c
MarcGames1 <George.marcu20@gmail.com>
2023-04-28 12:53:48 +0300

    Update __document.tsx

--- pages/__document.tsx
+++ pages/__document.tsx
@@ -24,23 +24,31 @@ export default class MyDocument extends Document {
 
   render(): ReactElement {
     return (
-      <Html lang='ro_RO'>
+      <Html lang="ro_RO">
         <Head>
           {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P4QDK6G');`}}></script>
+})(window,document,'script','dataLayer','GTM-5D4CHRX');`,
+            }}
+          ></script>
         </Head>
         <body>
-       
-        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4QDK6G"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5D4CHRX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
       </Html>
-   );
+    );
   }
 }
+
