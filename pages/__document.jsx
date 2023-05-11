023c1ee8f448244dd2ee48e2997e7acb3ec800ba
MarcGames1 <George.marcu20@gmail.com>
2023-05-11 11:11:06 +0300

    Update __document.jsx

--- pages/__document.jsx
+++ pages/__document.jsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   
