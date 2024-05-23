5522e00b4e46699901d69bdf7d3af0e7dc33244b
MarcGames1 <George.marcu20@gmail.com>
2024-05-23 08:52:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,65 +1,62 @@
-
 module.exports = {
-  distDir: 'out',
-  
-    async redirects() {
-      return [
-        {
-          source: '/consultantaOnline',
-          destination: '/avocat-online',
-          permanent: true,
-        },
-        {
-          source: '/dreptpenal',
-          destination: '/drept-penal',
-          permanent: true,
-        },
-        
-        {
-          source: '/dreptulfamiliei',
-          destination: '/dreptul-familiei',
-          permanent: true,
-        },
-        {
-          source: '/dreptcivil',
-          destination: '/drept-civil',
-          permanent: true,
-        },
-        {
-          source: '/MalpraxisMedical',
-          destination: '/malpraxis-medical',
-          permanent: true,
-        },
-        {
-          source: '/DreptulMuncii',
-          destination: '/dreptul-muncii',
-          permanent: true,
-        },
-        {
-          source: '/ExecutariSilite',
-          destination: '/executari-silite',
-          permanent: true,
-        },
-        {
-          source: '/DreptImobiliar',
-          destination: '/drept-imobiliar',
-          permanent: true,
-        },
-        {
-          source: '/DreptContraventional',
-          destination: '/drept-contraventional',
-          permanent: true,
-        },
-        {
-          source: '/DreptBancar',
-          destination: '/drept-bancar',
-          permanent: true,
-        },
-        {
-          source: '/DreptAdministrativ',
-          destination: '/drept-administrativ',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  async redirects() {
+    return [
+      {
+        source: "/consultantaOnline",
+        destination: "/avocat-online",
+        permanent: true,
+      },
+      {
+        source: "/dreptpenal",
+        destination: "/drept-penal",
+        permanent: true,
+      },
+
+      {
+        source: "/dreptulfamiliei",
+        destination: "/dreptul-familiei",
+        permanent: true,
+      },
+      {
+        source: "/dreptcivil",
+        destination: "/drept-civil",
+        permanent: true,
+      },
+      {
+        source: "/MalpraxisMedical",
+        destination: "/malpraxis-medical",
+        permanent: true,
+      },
+      {
+        source: "/DreptulMuncii",
+        destination: "/dreptul-muncii",
+        permanent: true,
+      },
+      {
+        source: "/ExecutariSilite",
+        destination: "/executari-silite",
+        permanent: true,
+      },
+      {
+        source: "/DreptImobiliar",
+        destination: "/drept-imobiliar",
+        permanent: true,
+      },
+      {
+        source: "/DreptContraventional",
+        destination: "/drept-contraventional",
+        permanent: true,
+      },
+      {
+        source: "/DreptBancar",
+        destination: "/drept-bancar",
+        permanent: true,
+      },
+      {
+        source: "/DreptAdministrativ",
+        destination: "/drept-administrativ",
+        permanent: true,
+      },
+    ];
+  },
+};
